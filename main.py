88f822827622c801464875a99222971a9b074ca4
Alfonso Saavedra "Son Link <sonlink.dourden@gmail.com>
2023-01-30 16:54:59 +0100

    Add gamepad support and litte performance

--- main.py
+++ main.py
@@ -3,14 +3,6 @@ from random import randint
 
 example = []
 
-# Genera una pantalla de prueba de manera aleatoria.
-for y in range(6):
-    line = []
-    for x in range(13):
-        line.append(randint(1, 11))
-
-    example.append(line)
-
 # Constantes del juego
 VELX = 2
 VELY = 2
@@ -22,6 +14,7 @@ COL_PADDLE = 5
 LIVES = 3
 SCREEN_W = 120
 SCREEN_H = 144
+
 
 class Ball():
     def __init__(self, paddle):
@@ -38,15 +31,16 @@ class Ball():
         pyxel.blt(self.posx, self.posy, 0, 32, 8, 4, 4)
 
     def update(self):
-        # Vamos a comprobar si choca contra el padd
+        # Vamos a comprobar si choca contra el pad
         if (
-            self.posyy + VELY >= 128 and
+            self.posyy + VELY >= 130 and
+            self.posy < 128 and
             self.posx >= self.paddle.posx and
             self.posxx <= self.paddle.posx + 16
         ):
             self.vely = 0 - VELY
 
-        # Comprobamos si esta llegando a algun sprite
+        # Comprobamos si esta llegando a algún sprite
         cols, tiles = self._checkColDir()
         if len(cols) > 0:
             for col in cols:
@@ -71,7 +65,7 @@ class Ball():
         self.posxx += self.velx
         self.posy += self.vely
         self.posyy += self.vely
-        
+
     def getPosition(self):
         return (self.posx, self.posy, self.posxx, self.posyy)
 
@@ -138,23 +132,24 @@ class Paddle():
         pyxel.blt(self.posx, self.posy, 0, 40, 8, 16, 4)
 
     def update(self):
-        if pyxel.btn(pyxel.KEY_LEFT) and self.posx > 8:
+        if pyxel.btn(pyxel.KEY_LEFT) or pyxel.btn(pyxel.GAMEPAD1_BUTTON_DPAD_LEFT) and self.posx > 8:
             self.posx -= 2
-        elif pyxel.btn(pyxel.KEY_RIGHT) and self.posx < 96:
+        elif pyxel.btn(pyxel.KEY_RIGHT) or pyxel.btn(pyxel.GAMEPAD1_BUTTON_DPAD_RIGHT) and self.posx > 8:
             self.posx += 2
 
     def getPosition(self):
         return (self.posx, self.posy)
-    
+
     def resetPos(self):
         self.posx = (SCREEN_W / 2) - 8
         self.posy = 128
+
 
 class App():
     def __init__(self):
         self.paddle = Paddle()
         self.ball = Ball(self.paddle)
-        
+
         # 1: Pantalla de inicio. 2: Jugando. 3: Muerte
         self.game_state = 1
         self.move_ball = False
@@ -165,7 +160,7 @@ class App():
 
     def update(self):
         global LIVES
-        
+
         # Primero comprobamos si la bola se sale por abajo
         ballPos = self.ball.getPosition()
         if ballPos[1] >= SCREEN_H:
@@ -173,13 +168,14 @@ class App():
             self.ball.resetBall()
             self.paddle.resetPos()
             self.move_ball = False
-            
+
         if LIVES == 0:
             self.game_state = 3
             self.move_ball = True
 
-        if pyxel.btnp(pyxel.KEY_SPACE):
+        if pyxel.btnp(pyxel.KEY_SPACE) or pyxel.btnp(pyxel.GAMEPAD1_BUTTON_A):
             if self.game_state == 1:
+                self.genWalls()
                 self.game_state = 2
             elif self.game_state == 2 and not self.move_ball:
                 self.move_ball = True
@@ -187,7 +183,7 @@ class App():
                 LIVES = 3
                 self.game_state = 1
                 self.move_ball = False
-        
+
         if self.game_state == 2:
             self.paddle.update()
             if self.move_ball:
@@ -203,16 +199,16 @@ class App():
 
         if self.game_state == 1:
             pyxel.text((SCREEN_W / 2) - 15, 32, 'ARKAWAY', 10)
-            pyxel.text((SCREEN_W / 2) - 40, 48, 'Press Space to start', 3)
+            pyxel.text(8, 48, 'Press Space key or A button to start', 3)
         elif self.game_state == 2:
             pyxel.bltm(0, 0, 0, 0, 0, 120, 144)
-            pyxel.text(4, 4, 'LIVES: {}'.format(LIVES), 9)
+            pyxel.text(4, 4, 'LIVES', 9)
             lives_start = 32
-            
+
             for i in range(LIVES):
                 pyxel.blt(lives_start, 3, 0, 56, 8, 8, 8)
                 lives_start += 8
-            
+
             for y in range(len(example)):
                 for x in range(13):
                     sprite = example[y][x]
@@ -226,6 +222,16 @@ class App():
             self.paddle.draw()
         elif self.game_state == 3:
             pyxel.text((SCREEN_W / 2) - 18, 32, 'GAME OVER', 8)
+
+    def genWalls(self):
+        global example
+        example = []
+        for y in range(6):
+            line = []
+            for x in range(13):
+                line.append(randint(1, 11))
+
+            example.append(line)
 
 
 App()
