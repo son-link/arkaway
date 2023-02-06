e4ed99a9defe85247df9c170807a54947110ff5d
Alfonso Saavedra "Son Link <sonlink.dourden@gmail.com>
2023-02-06 17:17:50 +0100

    Added maps.json to the build script

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 if [ ! -d package ]; then mkdir package; fi
 
 echo "Prepare the package"
-cp main.py assets.pyxres package
+cp main.py assets.pyxres maps.json package
 cd package
 pyxel package . main.py
 mv package.pyxapp ../arkaway.pyxapp
